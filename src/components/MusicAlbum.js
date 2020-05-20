709c212145349df1eb87268ad4614587815fcab8
janne-remes <janneremes@outlook.com>
2020-05-20 14:38:52 +0300

    Osiota 3 tehty: MusicAlbum.js -tiedostossa luokan luominen mennyt todennäköisesti väärille urille (ainakin kun vertasin omaa koodia puhekanavalla tehdyn esityksen yhteydessä toisen henkilön koodiin).

--- src/components/MusicAlbum.js
+++ src/components/MusicAlbum.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class MusicAlbum extends Component {
+    constructor() {
+        super()
+    
+        this.state = {
+             albumName: "Toxicity",
+             performerName: "System of a Down",
+             genre: "Alternative metal",
+             yearPublished: 2001
+        }
+    }
+    
+    //constructor(albumName, performerName, genre, yearPublished){
+    //    this.albumName = albumName;
+    //    this.performerName = performerName;
+    //    this.genre = genre;
+    //    this.yearPublished = yearPublished;
+    //};
+
+    //const albumOme = new MusicAlbum("Toxicity", "System of a Down", "Alternative metal", 2001);
+    
     render() {
+
         return (
             <div>
-                <h2>Music Album</h2>
+                <h3 className="details">Album name: <span className="details details-bolded">{this.state.albumName}</span></h3>
+                <h3 className="details">Performer: <span className="details details-bolded">{this.state.performerName}</span></h3>
+                <h3 className="details">Genre: <span className="details details-bolded">{this.state.genre}</span></h3>
+                <h3 className="details">Year Published: <span className="details details-bolded">{this.state.yearPublished}</span></h3>
             </div>
         );
     }
